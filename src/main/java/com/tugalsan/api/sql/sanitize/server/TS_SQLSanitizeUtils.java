90c9b37d7f6080c4aeaa8b6e041fb8b9eef5c7b6
tugalsan karabacak <tugalsan@gmail.com>
2022-06-20 23:35:05 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/api/sql/sanitize/server/TS_SQLSanitizeUtils.java
@@ -0,0 +1,46 @@
+package com.tugalsan.api.sql.sanitize.server;
+
+import com.tugalsan.api.list.client.*;
+import java.util.*;
+
+public class TS_SQLSanitizeUtils {
+
+    public static void sanitize(Object word, CharSequence banned) {
+        if (word == null || banned == null) {
+            return;
+        }
+        if (String.valueOf(word).contains(banned)) {
+            throw new RuntimeException(TS_SQLSanitizeUtils.class.getSimpleName() + ".sanitize tag [" + word + "] contains [" + banned + "]");
+        }
+    }
+
+    public static void sanitize(Object word) {
+        if (word == null) {
+            return;
+        }
+        var wordStr = String.valueOf(word);
+        if (TGS_ListUtils.of(
+                "INTEGER NOT NULL",
+                "VARCHAR(254) NOT NULL",
+                "INT NOT NULL"
+        ).contains(wordStr)) {
+            return;
+        }
+        TGS_ListUtils.of(",", ";", "+", "-", "*", "/", "\\", "{", "}", "[", "]", "=", "!", "<", ">", "%", " ")
+                .forEach(banned -> sanitize(word, banned));
+    }
+
+    public static void sanitize(List words) {
+        if (words == null) {
+            return;
+        }
+        words.forEach(word -> sanitize(word));
+    }
+
+    public static void sanitize(Object[] words) {
+        if (words == null) {
+            return;
+        }
+        Arrays.stream(words).forEach(word -> sanitize(word));
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.tugalsan.api.sql.sanitize {
+    requires com.tugalsan.api.list;
+    exports com.tugalsan.api.sql.sanitize.server;
+}
